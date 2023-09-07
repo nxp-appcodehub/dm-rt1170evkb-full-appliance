f12012de2130868da1db1d9cfb9739d0d54bd597
WBI\nxf93737 <alexis.luna@nxp.com>
2023-09-07 15:18:21 -0600

    Software Release V1.6

--- AirconGUI/lvgl-simulator/build/object/lv_utils.d
+++
@@ -1,5 +0,0 @@
-build/object/lv_utils.o: \
- c:\users\nxf93737\documents\repos\full_appliance_1170\rt1170-graphics-voice-ui\aircongui\lvgl\src\misc\lv_utils.c \
- c:\users\nxf93737\documents\repos\full_appliance_1170\rt1170-graphics-voice-ui\aircongui\lvgl\src\misc\lv_utils.h
-
-c:\users\nxf93737\documents\repos\full_appliance_1170\rt1170-graphics-voice-ui\aircongui\lvgl\src\misc\lv_utils.h:
